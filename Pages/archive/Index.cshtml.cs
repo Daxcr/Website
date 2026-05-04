191d4d58f4450811296fde639165cc4dffc9b2ae
Daxcr <daxaroodles@gmail.com>
2026-05-04 13:57:59 +1200

    Added Archive!

---
+++ Pages/archive/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Website.Pages.archive;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
